98031acfe9551a5919ec701f027fcf4b47badee4
Vandiel <52209920+vandiel01@users.noreply.github.com>
2025-04-15 01:30:31 -0500

    Add files via upload
    - Tweak iL/Gear/x/x to improve display
    - moved info from top to side, now displays: valorstone on top, how many you have, how many of each crest, then on bottom is the order of gear "upgrade" you will need to get to next one.

--- vC_GearStats.lua
+++ vC_GearStats.lua
@@ -4,6 +4,7 @@
 -- Declare for this Page
 -------------------------------------------------------
 local vC_GS_Slot = {
+	-- Type, L/R Col, Future Add (Ench, Embor, Gem, Tinker)?
 	{ "HeadSlot", 0, },
 	{ "NeckSlot", 0, },
 	{ "ShoulderSlot", 0, },
@@ -32,38 +33,40 @@ local vC_GS_Quality = {
 	{ 7, "Heirloom", "00CCFF", },
 	{ 8, "WoW Token", "00CCFF", },
 }
+local vC_GS_TWW_CurrList = {
+	{ 3008, "0070DD", "Valorstone", },
+	{ 3107, "58D68D", "Weathered", },
+	{ 3108, "5DADE2", "Carved", },
+	{ 3109, "BB8FCE", "Runed", },
+	{ 3110, "E59866", "Glided", },
+	{ 0, "58D68D", "Explorer", },
+	{ 0, "5DADE2", "Adventure", },
+	{ 0, "BB8FCE", "Veteran", },
+	{ 0, "BB8FCE", "Champion", },
+	{ 0, "E59866", "Hero", },
+	{ 0, "E59866", "Mythic", },
+}
+-------------------------------------------------------
+-- Simplified SetBackdrop
+-------------------------------------------------------
+function vC_GS_BackDrop(f, b, e, t)
+	f:SetBackdrop({
+		bgFile = (type(b) == "number" and b or "Interface\\" .. b), tileEdge = true, tileSize = 16,
+		edgeFile = (type(e) == "number" and e or "Interface\\" .. e), edgeSize = (t ~= nil and t or 16),
+		insets = { left = 4, right = 4, top = 4, bottom = 4 },
+	})
+end
 -------------------------------------------------------
 -- Simplified Gear iLevel in Character Pane
 -------------------------------------------------------
 function vC_GS_Display_iLevels()
-	if ( InCombatLockdown() or not CharacterFrame:IsShown() ) then return end
+	if ( not CharacterFrame:IsShown() ) then return end
 
 	local overall, equipped = GetAverageItemLevel()
 		CharacterStatsPane.ItemLevelFrame.Value:SetFontObject("SystemFont_Shadow_Huge1_Outline")
 		CharacterStatsPane.ItemLevelFrame.Value:SetText(format("%.2f",equipped))
-
-	local vC_Gear_Total = CharacterStatsPane:CreateFontString(nil, "ARTWORK", "SystemFont_Shadow_Med1_Outline")
-		vC_Gear_Total:SetPoint("BOTTOM", CharacterStatsPane, "BOTTOM", 0, 10)
-		vC_Gear_Total:SetTextColor(.71, .71, .71, .90)
-		vC_Gear_Total:SetText("|cffF8C471"..format("%.3f",equipped).."|r / |cffF1948A"..format("%.3f",overall).."|r")
-	local vC_Gear_Hint = CharacterStatsPane:CreateTexture(nil, "ARTWORK")
-		vC_Gear_Hint:SetSize(24, 24)
-		vC_Gear_Hint:SetTexture(616343)
-		vC_Gear_Hint:SetPoint("LEFT", vC_Gear_Total, "RIGHT", 0, 0)
-		vC_Gear_Hint:SetScript("OnEnter", function(s)
-			GameTooltip:ClearLines()
-			GameTooltip:SetOwner(s, "ANCHOR_RIGHT")
-			GameTooltip:AddLine(
-				"|cffFFFFFF"..(C_AddOns.GetAddOnMetadata("vC_GearStats", "Title"))..
-				" v"..(C_AddOns.GetAddOnMetadata("vC_GearStats", "Version")).."|r\n\n"..
-				"|cffF8C471Equipped|r / |cffF1948AOverall|r\n\n"..
-				"Calculates Average from:\n"..
-				"|cffF8C471Equipped|r: Equipped Only\n"..
-				"|cffF1948AOverall|r: Equipped and in Bag\n"
-			)
-			GameTooltip:Show()
-		end)
-		vC_Gear_Hint:SetScript("OnLeave", function(s) GameTooltip:Hide() end)
+		
+	vC_GS_Total:SetText("|cffF8C471" .. format("%.2f",equipped) .. "|r / |cffF1948A" .. format("%.2f",overall) .. "|r")	
 
 	local vC_ItemLevel = "Item Level (%d+)"
 	local vC_GearSeason = "(%w+) Season (%d+)"
@@ -75,78 +78,117 @@ function vC_GS_Display_iLevels()
 		local itemLink = GetInventoryItemLink("player", slotId) or 0
 
 		if ( itemLink ) then
-			local vC_t = CreateFrame("GameTooltip", "vC_t"..a, nil, "GameTooltipTemplate")
+			local vC_t = CreateFrame("GameTooltip", "vC_t" .. a, nil, "GameTooltipTemplate")
 				vC_t:ClearLines()
 				vC_t:SetOwner(UIParent, "ANCHOR_NONE")
 				vC_t:SetHyperlink(itemLink)
 
 			local x, y, z = 0, "", nil
 			for b = 2, vC_t:NumLines() do
-				local s = _G["vC_t"..a.."TextLeft"..b]:GetText():gsub("|cFF808080",""):gsub("|r", "")
+				local s = _G["vC_t" .. a .. "TextLeft" .. b]:GetText():gsub("|cFF808080",""):gsub("|r", "")
 				if (s:find("Sell Price") or s:find("Durability") or s:find("Binds when") or s:find("Binds to")) then break end
-
 				if ( s:find(vC_GearSeason) ) then x = tonumber(s:match("%d+")) end
-				
 				if ( s:find(vC_ItemLevel) ) then y = tonumber(s:match("%d+")) end
-				
 				if ( s:find(vC_UpgradeLevel) ) then
 					z = s:match("%w+ %d+\/%d+")
 					z = z:gsub("Explorer","E"):gsub("Adventurer","A"):gsub("Veteran","V"):gsub("Champion","C"):gsub("Hero","H"):gsub("Mythic","M")
 				end
 			end
-			_G["vC_GS_iL_"..vC_GS_Slot[a][1]]:SetText("|cFF"..(x == 1 and vC_GS_Quality[1][3] or vC_GS_Quality[quality+1][3])..y.."|r")			
-			_G["vC_GS_uG_"..vC_GS_Slot[a][1]]:SetText(z)
+			_G["vC_GS_iL_" .. vC_GS_Slot[a][1]]:SetText("|cff" .. (x == 1 and vC_GS_Quality[1][3] or vC_GS_Quality[quality+1][3]) .. y .. "|r")
+			_G["vC_GS_uG_" .. vC_GS_Slot[a][1]]:SetText(z)
+		end
+	end
+	
+	
+	for c = 1, #vC_GS_TWW_CurrList do
+		local vC_GS_tCLn = _G["vC_GS_CNum_" .. vC_GS_TWW_CurrList[c][3]]
+		if ( vC_GS_TWW_CurrList[c][1] ~= 0 ) then
+			vC_GS_tCLn:SetText(C_CurrencyInfo.GetCurrencyInfo(vC_GS_TWW_CurrList[c][1]).quantity )
 		end
 	end
 end
 -------------------------------------------------------
 -- Build Basic Frame/Label for Character Pane @ OnLoad
+-- Armor Upgrade Level, Crest Use Order
 -------------------------------------------------------
-for a = 1, #vC_GS_Slot do
-	local vC_GS_SlotF = "vC_GS_"..vC_GS_Slot[a][1]
-	if ( _G[vC_GS_SlotF] == nil ) then
-		local vC_GS_SlotF = CreateFrame("Frame", vC_GS_SlotF, CharacterStatsPane, "BackdropTemplate")
+if ( vC_GS_Total == nil ) then
+	local vC_GS_Total = CharacterStatsPane:CreateFontString("vC_GS_Total", "ARTWORK", "SystemFont_Shadow_Med1_Outline")
+		vC_GS_Total:SetPoint("BOTTOM", CharacterStatsPane, "BOTTOM", 0, 10)
+		vC_GS_Total:SetTextColor(.71, .71, .71, .90)
+		vC_GS_Total:SetText("|cffF8C4710.00|r / |cffF1948A0.00|r")
+			vC_Gear_Hint = CharacterStatsPane:CreateTexture(nil, "ARTWORK")
+			vC_Gear_Hint:SetSize(24, 24)
+			vC_Gear_Hint:SetTexture(616343)
+			vC_Gear_Hint:SetPoint("LEFT", vC_GS_Total, "RIGHT", 0, 0)
+			vC_Gear_Hint:SetScript("OnEnter", function(s)
+				GameTooltip:ClearLines()
+				GameTooltip:SetOwner(s, "ANCHOR_RIGHT")
+				GameTooltip:AddLine(
+					"|cffFFFFFF" .. (C_AddOns.GetAddOnMetadata("vC_GearStats", "Title")) ..
+					" v" .. (C_AddOns.GetAddOnMetadata("vC_GearStats", "Version")) .. "|r\n\n" ..
+					"|cffF8C471Equipped|r / |cffF1948AOverall|r\n\n" ..
+					"Calculates Average from:\n" ..
+					"|cffF8C471Equipped|r: Equipped Only\n" ..
+					"|cffF1948AOverall|r: Equipped and in Bag\n"
+				)
+				GameTooltip:Show()
+			end)
+			vC_Gear_Hint:SetScript("OnLeave", function(s) GameTooltip:Hide() end)
+
+	for a = 1, #vC_GS_Slot do
+		local vC_GS_SlotF = "vC_GS_" .. vC_GS_Slot[a][1]
+			vC_GS_SlotF = CreateFrame("Frame", vC_GS_SlotF, CharacterStatsPane, "BackdropTemplate")
 			vC_GS_SlotF:SetSize(42,28)
 			vC_GS_SlotF:SetPoint(
-				(vC_GS_Slot[a][2] == 0 and "TOPLEFT" or "TOPRIGHT"),
-				"Character"..vC_GS_Slot[a][1],
-				(vC_GS_Slot[a][2] == 0 and "TOPRIGHT" or "TOPLEFT"),
-				(vC_GS_Slot[a][2] == 0 and 8 or -8),
-				((a == 15 or a == 16) and -16 or -7)
+				vC_GS_Slot[a][2] == 0 and "TOPLEFT" or "TOPRIGHT",
+				"Character" .. vC_GS_Slot[a][1],
+				vC_GS_Slot[a][2] == 0 and "TOPRIGHT" or "TOPLEFT",
+				vC_GS_Slot[a][2] == 0 and 8 or -8,
+				(a == 15 or a == 16) and -16 or -7
 			)
 			vC_GS_SlotF:SetFrameStrata("HIGH")
-		local vC_GS_T = _G["vC_GS_iL_"..vC_GS_Slot[a][1]]
-			vC_GS_T = vC_GS_SlotF:CreateFontString("vC_GS_iL_"..vC_GS_Slot[a][1], "ARTWORK", "GameFontNormalLargeOutline")
+		local vC_GS_T = _G["vC_GS_iL_" .. vC_GS_Slot[a][1]]
+			vC_GS_T = vC_GS_SlotF:CreateFontString("vC_GS_iL_" .. vC_GS_Slot[a][1], "ARTWORK", "GameFontNormalLargeOutline")
 			vC_GS_T:SetPoint((vC_GS_Slot[a][2] == 0 and "TOPLEFT" or "TOPRIGHT"), vC_GS_SlotF)
 			vC_GS_T:SetJustifyH((vC_GS_Slot[a][2] == 0 and "LEFT" or "RIGHT"))
 			vC_GS_T:SetText(0)
-		local vC_GS_B = _G["vC_GS_uG_"..vC_GS_Slot[a][1]]
-			vC_GS_B = vC_GS_SlotF:CreateFontString("vC_GS_uG_"..vC_GS_Slot[a][1], "ARTWORK", "GameFontWhite")
+		local vC_GS_B = _G["vC_GS_uG_" .. vC_GS_Slot[a][1]]
+			vC_GS_B = vC_GS_SlotF:CreateFontString("vC_GS_uG_" .. vC_GS_Slot[a][1], "ARTWORK", "GameFontWhite")
 			vC_GS_B:SetPoint((vC_GS_Slot[a][2] == 0 and "BOTTOMLEFT" or "BOTTOMRIGHT"), vC_GS_SlotF)
 			vC_GS_B:SetJustifyH((vC_GS_Slot[a][2] == 0 and "LEFT" or "RIGHT"))
 			vC_GS_B:SetText(0)
 	end
-end
--------------------------------------------------------
--- Armor Upgrade Level, Crest Use Order
--------------------------------------------------------
-if ( vC_GS_UpgL_NxtCUpg == nil ) then
-	local vC_GS_UpgL_NxtCUpg = CreateFrame("Frame", "vC_GS_UpgL_NxtCUpg", CharacterFrame, "BackdropTemplate")
-		vC_GS_UpgL_NxtCUpg:SetSize(445, 40)
-		vC_GS_UpgL_NxtCUpg:SetBackdrop({
-			bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark", tileEdge = true, tileSize = 16,
-			edgeFile = "Interface\\ToolTips\\UI-Tooltip-Border", edgeSize = 16,
-			insets = { left = 4, right = 4, top = 4, bottom = 4 },
-		})
-		vC_GS_UpgL_NxtCUpg:SetPoint("TOPLEFT", CharacterFrame, "TOPLEFT", 40, 38)
 
-	local vC_GS_Nxt_Upg = vC_GS_UpgL_NxtCUpg:CreateFontString("vC_GS_Nxt_Upg", "ARTWORK", "GameFontNormalOutline")
-		vC_GS_Nxt_Upg:SetPoint("LEFT", vC_GS_UpgL_NxtCUpg, "LEFT", 10, 0)
-		vC_GS_Nxt_Upg:SetText(
-			"|cFFFFFFFFGears|r: |cFF58D68DExplorer|r > |cFF5DADE2Adventure|r > |cFFBB8FCEVeteran|r > |cFFBB8FCEChampion|r > |cFFE59866Hero|r > |cFFE59866Mythic|r\n"..
-			"|cFFFFFFFFCrest|r: |cFF58D68DWeathered|r > |cFF5DADE2Carved|r > |cFFBB8FCERuned|r > |cFFE59866Glided|r"
-		)
-		vC_GS_Nxt_Upg:SetJustifyH("LEFT")
+	local vC_GS_TWW_GearUpg = CreateFrame("Frame", "vC_GS_TWW_GearUpg", CharacterFrame, "BackdropTemplate")
+		vC_GS_TWW_GearUpg:SetSize(135, 205)
+		vC_GS_BackDrop(vC_GS_TWW_GearUpg, 312922, 137057, 16)
+		vC_GS_TWW_GearUpg:SetPoint("TOPLEFT", CharacterFrame, "TOPRIGHT", 0, -24)
+
+		local RowCount = -10
+		for i = 1, #vC_GS_TWW_CurrList do
+			local vC_GS_CLn = "vC_GS_CNum_" .. vC_GS_TWW_CurrList[i][3]
+				vC_GS_CLn = vC_GS_TWW_GearUpg:CreateFontString("vC_GS_CNum_" .. vC_GS_TWW_CurrList[i][3], "ARTWORK", "GameFontWhite")
+				vC_GS_CLn:SetWidth(43)
+				vC_GS_CLn:SetPoint("TOPLEFT", vC_GS_TWW_GearUpg, "TOPLEFT", 0, RowCount)
+				vC_GS_CLn:SetJustifyH("RIGHT")
+				vC_GS_CLn:SetText(vC_GS_TWW_CurrList[i][1] == 0 and "" or C_CurrencyInfo.GetCurrencyInfo(vC_GS_TWW_CurrList[i][1]).quantity)
+			local vC_GS_CLt = "vC_GS_CTex_" .. vC_GS_TWW_CurrList[i][3]
+				vC_GS_CLt = vC_GS_TWW_GearUpg:CreateFontString("vC_GS_CTex_" .. vC_GS_TWW_CurrList[i][3], "ARTWORK", "GameFontWhite")
+				vC_GS_CLt:SetWidth(80)
+				vC_GS_CLt:SetPoint("TOPLEFT", vC_GS_TWW_GearUpg, "TOPLEFT", 50, RowCount)
+				vC_GS_CLt:SetJustifyH("LEFT")
+				vC_GS_CLt:SetText("|cff" .. vC_GS_TWW_CurrList[i][2] .. vC_GS_TWW_CurrList[i][3] .. "|r")
+				
+			if ( i == 1 or i == 5 ) then
+				RowCount = RowCount - 7
+				local vC_GS_Sep = vC_GS_TWW_GearUpg:CreateTexture("vC_GS_Sep")
+					vC_GS_Sep:SetSize(115, 2)
+					vC_GS_Sep:SetTexture(130871)
+					vC_GS_Sep:SetColorTexture(.8, .8, .8, .5)
+					vC_GS_Sep:SetPoint("TOPLEFT", vC_GS_TWW_GearUpg, "TOPLEFT", 9, RowCount-11)
+			end
+			RowCount = RowCount - 16
+		end
 end
 -------------------------------------------------------
 -- Auto Repair
@@ -158,14 +200,19 @@ function vC_GS_Repair_Gears()
 			if ( canRepair ) then
 				RepairAllItems(true) -- Use GBank Fund
 				local RemainingFund = GetGuildBankWithdrawMoney() - repairCost
-				local GFund = (not IsGuildLeader() and " ("..GetCoinTextureString(tonumber(RemainingFund),10)..")" or "")
-				DEFAULT_CHAT_FRAME:AddMessage("Auto Repair: "..GetCoinTextureString(repairCost,10)..GFund)
+				local GFund = (not IsGuildLeader() and " (" .. GetCoinTextureString(tonumber(RemainingFund),10) .. ")" or "")
+				DEFAULT_CHAT_FRAME:AddMessage("Auto Repair: " .. GetCoinTextureString(repairCost) .. GFund)
 			end
+		else
+			if ( canRepair and ( repairCost <= GetMoney() ) ) then
+				RepairAllItems()
+				DEFAULT_CHAT_FRAME:AddMessage("No GFund Avail, Using Your Gold Repair: " .. GetCoinTextureString(repairCost))
+			end		
 		end
 	else
 		if ( canRepair and ( repairCost <= GetMoney() ) ) then
 			RepairAllItems()
-			DEFAULT_CHAT_FRAME:AddMessage("Auto Repair: "..GetCoinTextureString(repairCost,10))
+			DEFAULT_CHAT_FRAME:AddMessage("Auto Repair: " .. GetCoinTextureString(repairCost))
 		end
 	end
 end
@@ -188,10 +235,10 @@ local vC_GS_RegEv = CreateFrame("Frame")
 	-- Repair Gear
 	if ( e == "MERCHANT_SHOW" ) then vC_GS_Repair_Gears() end
 	-- Equip Change Events
-	if ( e == "PLAYER_EQUIPMENT_CHANGED" ) then
-		if ( CharacterFrame:IsShown() ) then vC_GS_Display_iLevels() end
+	if ( e == "PLAYER_EQUIPMENT_CHANGED" and CharacterFrame:IsShown() ) then
+		vC_GS_Display_iLevels()
 	end
 end)
 hooksecurefunc("PaperDollFrame_UpdateStats", function()
-	if ( not InCombatLockdown() ) then vC_GS_Display_iLevels() end
+	if ( CharacterFrame:IsShown() ) then vC_GS_Display_iLevels() end
 end)
